2397f2dc5361b17ef3fec9d2d3aedd219d20e8b3
Dylan-Bacchus22 <dylan.bacchus22@my.stjohns.edu>
2026-02-25 21:39:34 -0500

    Update executor.c

--- executor.c
+++ executor.c
@@ -17,11 +17,7 @@
 int execute_command(char *command, char **args) {
     pid_t pid;
     int status;
-
-    // TODO 1: Fork a child process
-    // Use fork() to create a new process
-    // Store the return value in 'pid'
-    // Check if fork failed (pid < 0) and return -1 if so
+    
     pid = fork();
     
     if (pid < 0)
@@ -30,13 +26,6 @@ int execute_command(char *command, char **args) {
         return -1;
     }
         
-    
-
-    // TODO 2: Child process - Execute the command
-    // Check if we're in the child process (pid == 0)
-    // Call execvp(command, args) to transform into the target program
-    // If execvp returns, it failed - print error and exit(1)
-    // CRITICAL: Child must call exit(1), NOT return!
     if (pid == 0)
     {
         execvp(command, args);
@@ -44,12 +33,6 @@ int execute_command(char *command, char **args) {
         perror("execvp");
         exit(1);
     }
-    
-    // TODO 3: Parent process - Wait for child to complete
-    // Use waitpid(pid, &status, 0) to wait for the specific child
-    // Check if child exited normally with WIFEXITED(status)
-    // If yes, return the exit code with WEXITSTATUS(status)
-    // Otherwise return -1
     
     waitpid(pid, &status, 0);
     if (WIFEXITED(status)) 
